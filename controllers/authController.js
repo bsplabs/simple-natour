7b2c8b93511abdcc6b578c38cd08c40ec5578031
p-sittikorn <sittigornp57@email.nu.ac.th>
2022-09-25 20:33:55 +0700

    create user model and add jwt lib

---
+++ controllers/authController.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create(req.body);
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+
+  res.status(201).json({
+    status: 'success',
+    token: token,
+    data: {
+      user: newUser,
+    },
+  });
+});
